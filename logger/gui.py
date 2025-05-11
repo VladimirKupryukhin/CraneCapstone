3dcc23f725a4d9492b3e6bd761d78f506229d5d7
VladFigy <vladimirkupryukhin@gmail.com>
2025-05-11 12:34:17 -0700

    Read data from pico, parse, and send to gui

--- logger/gui.py
+++ logger/gui.py
@@ -8,16 +8,19 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from itertools import count
 from matplotlib.pyplot import figure
+from collections import deque
+from usbThread import SMRTData
+from collections import deque
 
 
 class App(tkinter.Tk):
-    def __init__(self):
+    def __init__(self, dataQueue):
         super().__init__()
         self.title("Crane Aerospace Capstone 2024-2025 Logger")    
         self.geometry("1000x500")
         
-        recordButton = Button(self, text = str("Record"))
-        recordButton.grid(row = 0, column = 0)
+        self.recordButton = Button(self, text = str("Record"))
+        self.recordButton.grid(row = 0, column = 0)
         
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
@@ -27,13 +30,13 @@ class App(tkinter.Tk):
         self.grid_rowconfigure(1, weight=1)
         self.grid_rowconfigure(2, weight=1)
         
-        smrt1 = SMRTWidget(self, "SMRT 1")
-        smrt2 = SMRTWidget(self, "SMRT 2")
-        smrt3 = SMRTWidget(self, "SMRT 3")
+        self.smrt1 = SMRTWidget(self, "SMRT 1")
+        self.smrt2 = SMRTWidget(self, "SMRT 2")
+        self.smrt3 = SMRTWidget(self, "SMRT 3")
         
-        smrt1.grid(row=1,column=0)
-        smrt2.grid(row=1,column=1)
-        smrt3.grid(row=1,column=2)
+        self.smrt1.grid(row=1,column=0)
+        self.smrt2.grid(row=1,column=1)
+        self.smrt3.grid(row=1,column=2)
         
         plt.style.use('fivethirtyeight')
         # values for first graph
@@ -42,39 +45,44 @@ class App(tkinter.Tk):
         
         index = count()
         
+        self.dataQueue:deque = dataQueue
         
         
-        aFigure = plt.figure(figsize=(5, 5), dpi=80)
-        canvas = FigureCanvasTkAgg(aFigure, master=self)
-        canvas.get_tk_widget().grid(row=2, column=0)
-        a = plt.gcf().subplots(1)
+        self.aFigureXVals = []
+        self.aFigureYVals = []
+        self.aFigure = plt.figure(figsize=(5, 5), dpi=80)
+        self.canvas = FigureCanvasTkAgg(self.aFigure, master=self)
+        self.canvas.get_tk_widget().grid(row=2, column=0)
+        self.a = plt.gcf().subplots(1)
         
-        a.cla()
-        a.plot(x_vals, y_vals)
-        aFigure.suptitle("SMRT1 Temperature")
+        # self.a.cla()
+        #self.a.plot(self.aFigureXVals, self.aFigureYVals)
+        self.aFigure.suptitle("SMRT1 Temperature")
         
-        bFigure = plt.figure(figsize=(5, 5), dpi=80)
-        canvas2 = FigureCanvasTkAgg(bFigure, master=self)
-        canvas2.get_tk_widget().grid(row=2, column=1)
-        b = plt.gcf().subplots(1)
+        # bFigure = plt.figure(figsize=(5, 5), dpi=80)
+        # canvas2 = FigureCanvasTkAgg(bFigure, master=self)
+        # canvas2.get_tk_widget().grid(row=2, column=1)
+        # b = plt.gcf().subplots(1)
         
-        b.cla()
-        b.plot(y_vals, x_vals)
-        bFigure.suptitle("SMRT2 Temperature")
+        # b.cla()
+        # b.plot(y_vals, x_vals)
+        # bFigure.suptitle("SMRT2 Temperature")
         
-        cFigure = plt.figure(figsize=(5, 5), dpi=80)
-        canvas2 = FigureCanvasTkAgg(cFigure, master=self)
-        canvas2.get_tk_widget().grid(row=2, column=2)
-        c = plt.gcf().subplots(1)
+        # cFigure = plt.figure(figsize=(5, 5), dpi=80)
+        # canvas2 = FigureCanvasTkAgg(cFigure, master=self)
+        # canvas2.get_tk_widget().grid(row=2, column=2)
+        # c = plt.gcf().subplots(1)
         
-        c.cla()
-        c.plot(y_vals, x_vals)
-        cFigure.suptitle("SMRT3 Temperature")
+        # c.cla()
+        # c.plot(y_vals, x_vals)
+        # cFigure.suptitle("SMRT3 Temperature")
         
 
         
         
         self.protocol("WM_DELETE_WINDOW", self.on_close)
+        
+        self.after(ms=10, func=self.updateGuiData)
         
         #frame = Frame(self, bg='lightblue', width=200, height=200)
         # frame.grid(row = 2, column = 2)
@@ -90,6 +98,40 @@ class App(tkinter.Tk):
         # Label(frame, text = "Voltage Value")#.grid(row = 3, column = 0)
         # Label(frame, text = "5.4")#.grid(row = 3, column = 1)
         
+    def updateGuiData(self):
+        try:
+            #print("Before")
+            
+            if self.dataQueue:    
+                data:SMRTData = self.dataQueue.popleft()
+                
+                self.smrt1.tempData["text"] = str(data.smrt1Temp)[:6]
+                self.smrt1.aOutData["text"] = str(data.smrt1A)[:6]
+                self.smrt1.bPosOutData["text"] = str(data.smrt1BPOS)[:6]
+                self.smrt1.bNegOutData["text"] = str(data.smrt1BNEG)[:6]
+                
+                self.smrt2.tempData["text"] = str(data.smrt2Temp)[:6]
+                self.smrt2.aOutData["text"] = str(data.smrt2A)[:6]
+                self.smrt2.bPosOutData["text"] = str(data.smrt2BPOS)[:6]
+                self.smrt2.bNegOutData["text"] = str(data.smrt2BNEG)[:6]
+                
+                self.smrt3.tempData["text"] = str(data.smrt3Temp)[:6]
+                self.smrt3.aOutData["text"] = str(data.smrt3A)[:6]
+                self.smrt3.bPosOutData["text"] = str(data.smrt3BPOS)[:6]
+                self.smrt3.bNegOutData["text"] = str(data.smrt3BNEG)[:6]
+                
+                self.aFigureYVals.append(data.smrt1Temp)
+                self.aFigureXVals.append(self.aFigureXVals.count + 1)
+                
+                self.a.cla()
+                self.a.plot(self.aFigureXVals, self.aFigureYVals)
+
+            
+            self.after(ms=10, func=self.updateGuiData)
+        except IndexError:
+            pass    
+        
+        
     def on_close(self):
         plt.close('all')
         self.destroy()
@@ -100,34 +142,34 @@ class SMRTWidget(tkinter.Frame):
     def __init__(self, parent, smrtText):
         super().__init__(master = parent, padx=5, pady=5)
         
-        smrtLabel = Label(self, text=smrtText, font='bold')
+        self.smrtLabel = Label(self, text=smrtText, font='bold')
         
-        tempLabel = Label(self, text='Temperature',justify=tkinter.LEFT)
-        tempData = Label(self, text='0.00')
+        self.tempLabel = Label(self, text='Temperature',justify=tkinter.LEFT)
+        self.tempData = Label(self, text='0.00')
         
-        aOutLabel = Label(self, text="3.3V Out (A OUT)",justify=tkinter.LEFT)
-        aOutData = Label(self, text='0.00')
+        self.aOutLabel = Label(self, text="3.3V Out (A OUT)",justify=tkinter.LEFT)
+        self.aOutData = Label(self, text='0.00')
         
-        bPosOutLabel = Label(self, text='+15V Out (B POS OUT)',justify=tkinter.LEFT)
-        bPosOutData = Label(self, text='0.00')
+        self.bPosOutLabel = Label(self, text='+15V Out (B POS OUT)',justify=tkinter.LEFT)
+        self.bPosOutData = Label(self, text='0.00')
         
-        bNegOutLabel = Label(self, text='-15V Out (B NEG OUT)',justify=tkinter.LEFT)
-        bNegOutData = Label(self, text='0.00')
+        self.bNegOutLabel = Label(self, text='-15V Out (B NEG OUT)',justify=tkinter.LEFT)
+        self.bNegOutData = Label(self, text='0.00')
         
         # Grids
-        smrtLabel.grid(row=0,column=0)
+        self.smrtLabel.grid(row=0,column=0)
         
-        tempLabel.grid(row=1,column=0)
-        tempData.grid(row=1,column=1)
+        self.tempLabel.grid(row=1,column=0)
+        self.tempData.grid(row=1,column=1)
         
-        aOutLabel.grid(row=2,column=0)
-        aOutData.grid(row=2,column=1)
+        self.aOutLabel.grid(row=2,column=0)
+        self.aOutData.grid(row=2,column=1)
         
-        bPosOutLabel.grid(row=3,column=0)
-        bPosOutData.grid(row=3,column=1)
+        self.bPosOutLabel.grid(row=3,column=0)
+        self.bPosOutData.grid(row=3,column=1)
         
-        bNegOutLabel.grid(row=4,column=0)
-        bNegOutData.grid(row=4,column=1)
+        self.bNegOutLabel.grid(row=4,column=0)
+        self.bNegOutData.grid(row=4,column=1)
         
         
 
--- logger/usbThread.py
+++ logger/usbThread.py
@@ -1,45 +1,148 @@
 import serial
 import time
 from serial.tools import list_ports
+from dataclasses import dataclass
 
 VID = 11914
 PID = 10
 
-def usbThreadEntry():
+def usbThreadEntry(dataQueue):
+    
     print("Starting USB Thread")
     
     conn = serial.Serial(str(getCOMPort()), 115200)
     #conn.timeout = 0.1
     print(conn.name)
     
+    picoInterface = PicoData(conn)
+    
     while True:
+        picoInterface.readData()
+        picoInterface.convertData()
+        picoInterface.printData()
+        
+        sendDataToGui(picoInterface, dataQueue)
+        time.sleep(0.5)
+        
+def sendDataToGui(picoInterface, dataQueue):
+    data = SMRTData(
+        picoInterface.smrt1Temp,
+        picoInterface.smrt1A,
+        picoInterface.smrt1BPOS,
+        picoInterface.smrt1BNEG,
+        picoInterface.smrt2Temp,
+        picoInterface.smrt2A,
+        picoInterface.smrt2BPOS,
+        picoInterface.smrt2BNEG,
+        picoInterface.smrt3Temp,
+        picoInterface.smrt3A,
+        picoInterface.smrt3BPOS,
+        picoInterface.smrt3BNEG)
+    
+    dataQueue.append(data)
+
+
+@dataclass
+class SMRTData:
+    smrt1Temp: int
+    smrt1A: int
+    smrt1BPOS: int
+    smrt1BNEG: int
+    
+    smrt2Temp: int
+    smrt2A: int
+    smrt2BPOS: int
+    smrt2BNEG: int
+    
+    smrt3Temp: int
+    smrt3A: int
+    smrt3BPOS: int
+    smrt3BNEG: int
+        
+class PicoData:
+    def __init__(self, connection):
+        self.smrt1Temp = -1
+        self.smrt1A = -1
+        self.smrt1BPOS = -1
+        self.smrt1BNEG = -1
+        
+        self.smrt2Temp = -1
+        self.smrt2A = -1
+        self.smrt2BPOS = -1
+        self.smrt2BNEG = -1
+        
+        self.smrt3Temp = -1
+        self.smrt3A = -1
+        self.smrt3BPOS = -1
+        self.smrt3BNEG = -1
+        
+        self.conn = connection
+        
+    def readData(self):
         get = "g\n"
-        conn.write(get.encode('utf-8'))
-        print("SENT:   " + str(get.encode('utf-8')))
-        #line1 = conn.read_until("\r\n".encode('utf-8'))
+        self.conn.write(get.encode('utf-8'))
         
-        print("READ:   " + str(conn.read_until("\r\n".encode('utf-8'))))
+        self.conn.read_until("\r\n".encode('utf-8'))
         
-        print("READ:   " + str(conn.read_until("\r\n".encode('utf-8'))))
-        print("READ:   " + str(conn.read_until("\r\n".encode('utf-8'))))
-        print("READ:   " + str(conn.read_until("\r\n".encode('utf-8'))))
-        print("READ:   " + str(conn.read_until("\r\n".encode('utf-8'))))
+        self.smrt1Temp = self.conn.read_until("\r\n".encode('utf-8')).decode("utf-8")
+        self.smrt1A = self.conn.read_until("\r\n".encode('utf-8')).decode("utf-8")
+        self.smrt1BPOS = self.conn.read_until("\r\n".encode('utf-8')).decode("utf-8")
+        self.smrt1BNEG = self.conn.read_until("\r\n".encode('utf-8')).decode("utf-8")
         
-        print("READ:   " + str(conn.read_until("\r\n".encode('utf-8'))))
-        print("READ:   " + str(conn.read_until("\r\n".encode('utf-8'))))
-        print("READ:   " + str(conn.read_until("\r\n".encode('utf-8'))))
-        print("READ:   " + str(conn.read_until("\r\n".encode('utf-8'))))
+        self.smrt2Temp = self.conn.read_until("\r\n".encode('utf-8')).decode("utf-8")
+        self.smrt2A = self.conn.read_until("\r\n".encode('utf-8')).decode("utf-8")
+        self.smrt2BPOS = self.conn.read_until("\r\n".encode('utf-8')).decode("utf-8")
+        self.smrt2BNEG = self.conn.read_until("\r\n".encode('utf-8')).decode("utf-8")
         
-        print("READ:   " + str(conn.read_until("\r\n".encode('utf-8'))))
-        print("READ:   " + str(conn.read_until("\r\n".encode('utf-8'))))
-        print("READ:   " + str(conn.read_until("\r\n".encode('utf-8'))))
-        print("READ:   " + str(conn.read_until("\r\n".encode('utf-8'))))
+        self.smrt3Temp = self.conn.read_until("\r\n".encode('utf-8')).decode("utf-8")
+        self.smrt3A = self.conn.read_until("\r\n".encode('utf-8')).decode("utf-8")
+        self.smrt3BPOS = self.conn.read_until("\r\n".encode('utf-8')).decode("utf-8")
+        self.smrt3BNEG = self.conn.read_until("\r\n".encode('utf-8')).decode("utf-8")
         
-        print("READ:   " + str(conn.read_until("\r\n".encode('utf-8'))))
+        self.conn.read_until("\r\n".encode('utf-8'))
         
-        print("\n")
-        time.sleep(1)
+        self.processData()
         
+    def processData(self):
+        self.smrt1Temp = self.smrt1Temp[6:-2]
+        self.smrt1A = self.smrt1A[6:-2]
+        self.smrt1BPOS = self.smrt1BPOS[6:-2]
+        self.smrt1BNEG = self.smrt1BNEG[6:-2]
+        
+        self.smrt2Temp = self.smrt2Temp[6:-2]
+        self.smrt2A = self.smrt2A[6:-2]
+        self.smrt2BPOS = self.smrt2BPOS[6:-2]
+        self.smrt2BNEG = self.smrt2BNEG[6:-2]
+        
+        self.smrt3Temp = self.smrt3Temp[6:-2]
+        self.smrt3A = self.smrt3A[6:-2]
+        self.smrt3BPOS = self.smrt3BPOS[6:-2]
+        self.smrt3BNEG = self.smrt3BNEG[6:-2]
+        
+    def printData(self):
+        print("\n----")
+        print("SMRT 1 TEMP: " + str(self.smrt1Temp))
+        print("SMRT 1 A: " + str(self.smrt1A))
+        print("SMRT 1 BPOS: " + str(self.smrt1BPOS))
+        print("SMRT 1 BNEG: " + str(self.smrt1BNEG))
+        
+        print("SMRT 2 TEMP: " + str(self.smrt2Temp))
+        print("SMRT 2 A: " + str(self.smrt2A))
+        print("SMRT 2 BPOS: " + str(self.smrt2BPOS))
+        print("SMRT 2 BNEG: " + str(self.smrt2BNEG))
+        
+        print("SMRT 3 TEMP: " + str(self.smrt3Temp))
+        print("SMRT 3 A: " + str(self.smrt3A))
+        print("SMRT 3 BPOS: " + str(self.smrt3BPOS))
+        print("SMRT 3 BNEG: " + str(self.smrt3BNEG))
+        
+    def convertData(self):
+        self.smrt1Temp = (3.3 * int(self.smrt1Temp)) / 4096
+        self.smrt2Temp = (3.3 * int(self.smrt2Temp)) / 4096
+        self.smrt3Temp = (3.3 * int(self.smrt3Temp)) / 4096
+        
+        #self.smrt1A = (3.3 * int(self.smrt1A)) / 4096 
+        pass
         
 # Gets the pi pico com port on windows  
 def getCOMPort():
